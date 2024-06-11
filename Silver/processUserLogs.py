87cd5db75560fae65d82d65c6bc0851409826d7f
andre-ls <andre.luis.e3@gmail.com>
2024-06-10 23:08:51 -0300

    Fix Negative Seconds on User Logs

--- Silver/processUserLogs.py
+++ Silver/processUserLogs.py
@@ -8,10 +8,14 @@ class ProcessUserLogs(Process):
 
     def formatDates(self, df):
         return df.withColumn("date", to_date(col("date").cast("string"), "yyyyMMdd"))
+
+    def filterInvalidSecs(self, df): #Remove rows with negative total_secs
+        return df.filter("total_secs >= 0")
     
     def run(self):
         df = self.readFromBronze()
         df = self.renameColumns(df)
         df = self.formatDates(df)
+        df = self.filterInvalidSecs(df)
         self.writeToSilver(df)
 
